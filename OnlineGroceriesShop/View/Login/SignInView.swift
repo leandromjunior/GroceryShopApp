fd3e0b09ca63b234614f6da4dbfebca5c3a611a5
Leandro Motta Junior <lemottajr@hotmail.com>
2025-03-20 00:26:24 -0300

    Inserted the top image

--- OnlineGroceriesShop/View/Login/SignInView.swift
+++ OnlineGroceriesShop/View/Login/SignInView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct SignInView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            
+            VStack{
+                Image("sign_in_top")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: .screenWidth, height: .screenWidth)
+                
+                Spacer() // Responsável, neste contexto, por colocar a imagem no topo
+            }
+            
+        }
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
+        .ignoresSafeArea() //Preenche a safe area com a imagem
     }
 }
 
