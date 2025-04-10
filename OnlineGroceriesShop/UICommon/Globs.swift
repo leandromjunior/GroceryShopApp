2f52b4912e884eefa4f6a31c2112cf5647a7cc54
Leandro Motta Junior <lemottajr@hotmail.com>
2025-04-10 20:59:10 -0300

    Criando a função para chamar a api de login da classe ServiceCall

---
+++ OnlineGroceriesShop/UICommon/Globs.swift
@@ -0,0 +1,18 @@
+//
+//  Globs.swift
+//  OnlineGroceriesShop
+//
+//  Created by Leandro Motta Junior on 10/04/25.
+//
+
+import SwiftUI
+
+struct Globs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Globs()
+}
--- OnlineGroceriesShop/ViewModel/MainViewModel.swift
+++ OnlineGroceriesShop/ViewModel/MainViewModel.swift
@@ -14,4 +14,32 @@ class MainViewModel: ObservableObject {
     @Published var txtPassword: String = ""
     @Published var showPassword: Bool = false // Se esta linha for setada como true, ao digitar a senha na tela de login, ela irá ser exibida num primeiro momento, sendo ocultada apenas se o usuário clicar no icone de olho
     
+    @Published var showError = false // Controla se a view deve receber um alerta de erro
+    @Published var errorMessage = "" // Mensagem de eerro
+    
+    // MARK: ServiceCall
+    
+    func serviceLogin() {
+        
+        // Enviando os dados de email e senha como parametro
+        ServiceCall.post(parameter: ["email": txtEmail, "password": txtPassword], path: Globs.SV_LOGIN) {responseObj in
+            
+            if let response = responseObj as? NSDictionary {
+                if response.value(forKey: KKey.status) as? String ?? "" == "1" {
+                    print(response)
+                    self.errorMessage = response.value(forKey: KKey.message) as? String ?? "Success"
+                    self.showError = true
+                }
+                else {
+                    self.errorMessage = response.value(forKey: KKey.message) as? String ?? "Fail"
+                    self.showError = true
+                }
+            }
+                
+        } failure: { error in
+            self.errorMessage = error?.localizedDescription ?? "Fail"
+            self.showError = true
+        }
+        
+    }
 }
