05bc421f7bdbdd8a97a8f38534509c5e22438b2b
Leandro Motta Junior <lemottajr@hotmail.com>
2025-04-10 21:02:32 -0300

    Creating the response API variables

--- OnlineGroceriesShop/UICommon/Globs.swift
+++ OnlineGroceriesShop/UICommon/Globs.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 
-struct Globs: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct Globs {
+    
+    static let AppName = ""
+    
+    static let BASE_URL = "http://localhost:3001/api/app/"
+    
+    static let SV_LOGIN = BASE_URL + "login"
 }
 
-#Preview {
-    Globs()
+struct KKey {
+    static let status = "status"
+    static let message = "message"
+    static let payload = "payload"
 }
